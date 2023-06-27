d566398474d2729a9b0de3a8b5ab6c18ea065ec6
Vladimir <vladimir.papin@gmail.com>
2023-06-27 20:51:43 +0300

    optimize popular posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
 
 
 def get_related_posts_count(tag):
@@ -7,7 +8,7 @@ def get_related_posts_count(tag):
 
 
 def get_lakes_posts_count(post):
-    return post.likes.count()
+    return post.num_likes
 
 
 def serialize_post(post):
@@ -33,7 +34,7 @@ def serialize_tag(tag):
 
 def index(request):
 
-    posts = Post.objects.all()
+    posts = Post.objects.annotate(num_likes=Count('likes'))
     popular_posts = sorted(posts, key=get_lakes_posts_count)
     most_popular_posts = list(popular_posts)[-5:]
 
